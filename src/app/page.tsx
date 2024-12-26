fc08a77f81482da5f20cf19e79500a1175842799
Fernando Tekisa <164080430+FernandoTekisa@users.noreply.github.com>
2024-12-26 21:13:40 +0100

    Design final
    O design final do site advice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 
 export default function Home() {
   return (
-    <main className="container h-screen w-screen mx-auto flex items-center justify-center">
-      <div className="flex flex-col bg-slate-200 shadow-lg shadow-gray-200 py-44 px-40 rounded">
+    <div className="container h-screen w-screen mx-auto flex items-center justify-center">
+      <div className="flex flex-col bg-slate-200 shadow-lg shadow-gray-300 py-3 px-4 mx-10 rounded-lg">
+        <div>
+          <h1 className="text-2xl font-semibold">ADVICE</h1>
+        </div>
+        <div className="flex mt-9 items-center justify-center">
+          <h2 className="text-lg font-medium">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
+        </div>
+        <div className="flex items-center justify-center mt-7">
+          <button className="bg-blue-500 py-2 px-5 rounded">
+            <span className="text-base font-normal text-slate-100">Seguinte→</span>
+          </button>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
