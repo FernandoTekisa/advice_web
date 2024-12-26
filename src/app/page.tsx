2a796210bc1667423e6bb5433f24c42504aa4709
Fernando Tekisa <164080430+FernandoTekisa@users.noreply.github.com>
2024-12-26 12:15:46 +0100

    alterando
    erro na ao desenvolver

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,7 @@
 export default function Home() {
   return (
     <main className="container h-screen w-screen mx-auto flex items-center justify-center">
-      <div className="flex flex-col bg-slate-200 shadow-lg shadow-gray-200 py-40 px-10 rounded-sm">
-        <nav className="flex">
-          <h1>ADVICE</h1>
-        </nav>
-        <div>
-          <p>Vive a vida com Deus dhdhubbehheudeuhyffgorfyfeuheu</p>
-        </div>
-        <div>
-          <button>Seguinte</button>
-        </div>
+      <div className="flex flex-col bg-slate-200 shadow-lg shadow-gray-200 py-44 px-40 rounded">
       </div>
     </main>
   );
